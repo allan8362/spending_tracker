147cf1a2a7ea2535f344cb457b0f25316a898bbf
Allan Brown <allanbrown8362@gmail.com>
2019-09-21 12:18:04 +0100

    completed CRUD methods for income class

--- models/income.rb
+++ models/income.rb
@@ -19,15 +19,34 @@ class Income
     @id = results.first()['id'].to_i
   end
 
+  def update()
+    sql = "UPDATE incomes SET (name, amount) = ($1, $2) WHERE id = $3"
+    values = [@name, @amount, @id]
+    SqlRunner.run(sql, values)
+  end
+
   def self.all()
     sql = "SELECT * FROM incomes"
     results = SqlRunner.run(sql)
     return results.map {|income| Income.new(income)}
   end
 
+  def self.find(id)
+    sql = "SELECT * FROM incomes WHERE id = $1"
+    values = [id]
+    income = SqlRunner.run(sql, values)
+    return Income.new(income.first())
+  end
+
   def self.delete_all()
     sql = "DELETE FROM incomes"
     SqlRunner.run(sql)
+  end
+
+  def delete()
+    sql = "DELETE FROM incomes WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
   end
 
 end
