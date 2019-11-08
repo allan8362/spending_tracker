c638a962f1306d0b06dfb51e5b688841e85c9fa8
Allan Brown <allanbrown8362@gmail.com>
2019-11-08 21:35:30 +0000

    Refactored month start and end date

--- models/month.rb
+++ models/month.rb
@@ -1,34 +1,24 @@
 require('date')
+require('pry-byebug')
 
 class Month
 
   def self.month_start(this_month)
 
     this_year = Date.today().year()
+    month = sprintf("%02d", this_month).to_i
+    new_date = Date.new(this_year, month, 1)
+    start_date = new_date.strftime("%Y-%m-%d")
 
-    case
-    when this_month < 10
-      start_date = "#{this_year}-0#{this_month}-01"
-    else
-      start_date = "#{this_year}-#{this_month}-01"
-    end
   end
 
   def self.month_end(this_month)
 
     this_year = Date.today().year()
-    case
-    when this_month == 1, 3, 5, 7, 8
-      end_date = "#{this_year}-0#{this_month}-31"
-    when this_month == 2
-      end_date = "#{this_year}-0#{this_month}-28"
-    when this_month == 4, 6, 9, 11
-      end_date = "#{this_year}-0#{this_month}-30"
-    when this_month == 11
-      end_date = "#{this_year}-#{this_month}-30"
-    when this_month == 10, 12
-      end_date = "#{this_year}-#{this_month}-31"
-    end
+    month = sprintf("%02d", this_month).to_i
+    new_date = Date.new(this_year, month, -1)
+    end_date = new_date.strftime("%Y-%m-%d")
+
   end
 
   def self.month_names()
